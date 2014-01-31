c57a352a365b942d14c1427b01f1042171165d8d
odiaseo <peleodiase@yahoo.co.uk>
2014-01-31 21:07:04 +0000

    Moved pagebuilder to listener class

--- Module.php
+++ Module.php
@@ -10,9 +10,7 @@
 namespace PageBuilder;
 
 
-use PageBuilder\View\Helper\FlashMessages;
-use PageBuilder\View\Helper\MicroData;
-use Zend\Console\Request;
+use PageBuilder\Event\Listener\PageBuilderListener;
 use Zend\Http\Response;
 use Zend\ModuleManager\Feature\DependencyIndicatorInterface;
 use Zend\Mvc\ModuleRouteListener;
@@ -28,13 +26,6 @@ class Module
         return array('SynergyDataGrid');
     }
 
-    public function init($moduleManager)
-    {
-        /** @var $moduleManager \Zend\ModuleManager\ModuleManager */
-        $sharedEvents = $moduleManager->getEventManager()->getSharedManager();
-        $sharedEvents->attach(__NAMESPACE__, MvcEvent::EVENT_DISPATCH, array($this, 'onModuleDispatch'), 103);
-        $sharedEvents->attach(__NAMESPACE__, MvcEvent::EVENT_DISPATCH, array($this, 'initEntityManager'), 103);
-    }
 
     public function onBootstrap(MvcEvent $e)
     {
@@ -42,53 +33,9 @@ class Module
         $eventManager        = $e->getApplication()->getEventManager();
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
+        $eventManager->attach(new PageBuilderListener());
     }
 
-
-    public function onModuleDispatch(MvcEvent $e)
-    {
-        if ($app = $e->getApplication()) {
-            $locator = $app->getServiceManager();
-            $config  = $locator->get('config');
-
-            if ($mainMenuKey = $config['pagebuilder']['main_navigation']) {
-
-                $viewHelperManager = $locator->get('viewHelperManager');
-
-                /** @var $navigation \Zend\View\Helper\Navigation */
-                $navigation = $viewHelperManager->get('navigation');
-
-                /** @var $menuTree \Zend\View\Helper\Navigation */
-                $menuTree  = $navigation($mainMenuKey);
-                $container = $menuTree->getContainer();
-
-                $activeMenu = $navigation->findActive($container);
-
-                if ($activeMenu) {
-                    /** @var $activeTheme \SynergyCommon\Entity\AbstractEntity */
-                    $activeTheme = $locator->get('active_theme');
-                    $menu        = $locator->get('pagebuilder\model\page')->findObject($activeMenu['page']->id);
-
-                    /** @var $pageBuilder \PageBuilder\View\Helper\PageBuilder */
-                    $pageBuilder = $viewHelperManager->get('buildPage');
-                    $pageBuilder->init($menu, $menuTree, $activeTheme);
-                }
-            }
-        }
-    }
-
-    public function initEntityManager(MvcEvent $e)
-    {
-        /** @var  $sm \Zend\Servicemanager\ServiceManager */
-        $sm = $e->getApplication()->getServiceManager();
-
-        /** @var $em \Doctrine\ORM\EntityManager */
-        $em = $sm->get('doctrine.entitymanager.orm_default');
-
-        /** @var $siteFilter \SynergyCommon\Doctrine\Filter\SiteFilter */
-        $siteFilter = $em->getFilters()->enable("site-specific");
-        $siteFilter->setServiceManager($sm);
-    }
 
     public function getConfig()
     {
@@ -111,12 +58,12 @@ class Module
 
         return array(
             'aliases'      => array(
-                'pages_service'     => __NAMESPACE__ . '\Service\PageService',
-                'component_service' => __NAMESPACE__ . '\Service\ComponentService',
-                'template_service'  => __NAMESPACE__ . '\Service\TemplateService',
-                'widget_service'    => __NAMESPACE__ . '\Service\WidgetService',
-                'pagebuilder\menu'  => __NAMESPACE__ . '\Navigation\NavigationFactory',
-                'util\widget'       => __NAMESPACE__ . '\Util\Widget',
+                'pages_service'     => 'PageBuilder\Service\PageService',
+                'component_service' => 'PageBuilder\Service\ComponentService',
+                'template_service'  => 'PageBuilder\Service\TemplateService',
+                'widget_service'    => 'PageBuilder\Service\WidgetService',
+                'pagebuilder\menu'  => 'PageBuilder\Navigation\NavigationFactory',
+                'util\widget'       => 'PageBuilder\Util\Widget',
             ),
             'initializers' => array(
                 'widget' => function ($widget, $sm) {
@@ -144,60 +91,19 @@ class Module
             ),
 
             'factories'    => array(
-                __NAMESPACE__ . '\Service\PageService'          => __NAMESPACE__ . '\Service\PageService',
-                __NAMESPACE__ . '\Navigation\NavigationFactory' => __NAMESPACE__ . '\Navigation\NavigationFactory',
-                __NAMESPACE__ . '\Service\PageService'          => __NAMESPACE__ . '\Service\PageService',
-                __NAMESPACE__ . '\Service\ComponentService'     => __NAMESPACE__ . '\Service\ComponentService',
-                __NAMESPACE__ . '\Service\WidgetService'        => __NAMESPACE__ . '\Service\WidgetService',
-                __NAMESPACE__ . '\Service\TemplateService'      => __NAMESPACE__ . '\Service\TemplateService',
-                __NAMESPACE__ . '\WidgetDataFactory'            => __NAMESPACE__ . '\WidgetDataFactory',
-
-                'active_theme'                                  => function ($sm) {
+                'active_site'                              => 'SynergyCommon\Service\ActiveClientSite',
+                'PageBuilder\Service\PageService'          => 'PageBuilder\Service\PageService',
+                'PageBuilder\Navigation\NavigationFactory' => 'PageBuilder\Navigation\NavigationFactory',
+                'PageBuilder\Service\ComponentService'     => 'PageBuilder\Service\ComponentService',
+                'PageBuilder\Service\WidgetService'        => 'PageBuilder\Service\WidgetService',
+                'PageBuilder\Service\TemplateService'      => 'PageBuilder\Service\TemplateService',
+                'PageBuilder\WidgetDataFactory'            => 'PageBuilder\WidgetDataFactory',
+                'active_theme'                             => function ($sm) {
                     /** @var  $sm \Zend\Servicemanager\ServiceManager */
                     $site  = $sm->get('active_site');
                     $theme = $sm->get('pagebuilder\model\theme')->getActiveTheme($site->getId());
 
                     return $theme;
-                },
-
-                'active_site'                                   => function ($sm) {
-                    /** @var  $sm \Zend\Servicemanager\ServiceManager */
-                    $containerKey = 'active-site';
-                    $request      = $sm->get('application')->getRequest();
-
-                    if ($request instanceof Request) {
-                        /** @var $event \Zend\Mvc\MvcEvent */
-                        $event = $sm->get('application')->getMvcEvent();
-                        /** @var $rm \Zend\Mvc\Router\RouteMatch */
-                        $rm   = $event->getRouteMatch();
-                        $host = $rm->getParam('host');
-                    } else {
-                        /** @var $request \Zend\Http\PhpEnvironment\Request */
-                        $host = $request->getServer('HTTP_HOST');
-                    }
-
-                    $hostname   = str_replace(array('http://', 'https://', 'www.'), '', $host);
-                    $sessionKey = preg_replace('/[^\p{L}\p{N}]+/ui', '', "host{$hostname}");
-
-                    /** @var $container \ArrayObject */
-                    $container = new Container($sessionKey);
-
-                    if ($container->offsetExists($containerKey)) {
-                        $em   = $sm->get('doctrine.entitymanager.orm_default');
-                        $site = $container->offsetGet($containerKey);
-                        $site = $em->merge($site);
-                    } elseif (!$site = $sm->get('pagebuilder\model\site')->findOneByDomain($hostname)) {
-                        header
-                        (
-                            'HTTP/1.1 403 Application Error'
-                        );
-                        echo "Site is not registered";
-                        exit;
-                    } else {
-                        $container->offsetSet($containerKey, $site);
-                    }
-
-                    return $site;
                 },
             )
         );
@@ -208,27 +114,11 @@ class Module
     {
         return array(
             'factories'  => array(
-                'flashMessages' => function ($sm) {
-                    /** @var  $sm \Zend\Servicemanager\ServiceLocatorAwareInterface */
-                    /** @var  $serviceManager \Zend\Servicemanager\ServiceManager */
-                    $serviceManager = $sm->getServiceLocator();
-                    $flashmessenger = $serviceManager->get('ControllerPluginManager')->get('flashmessenger');
-
-                    $messages = new FlashMessages();
-                    $messages->setFlashMessenger($flashmessenger);
-
-                    return $messages;
-                },
-                'microData'     => function ($sm) {
-                    $microData = new MicroData();
-                    $microData->setServiceManager($sm);
-
-                    return $microData;
-
-                },
+                'flashMessages' => 'SynergyCommon\View\Helper\FlashMessages',
+                'microData'     => 'SynergyCommon\View\Helper\MicroData',
             ),
             'invokables' => array(
-                'buildPage' => __NAMESPACE__ . '\View\Helper\PageBuilder',
+                'buildPage' => 'PageBuilder\View\Helper\PageBuilder',
             )
         );
     }
---
+++ src/PageBuilder/Event/Listener/PageBuilderListener.php
@@ -0,0 +1,74 @@
+<?php
+namespace PageBuilder\Event\Listener;
+
+use Zend\EventManager\EventManagerInterface;
+use Zend\EventManager\ListenerAggregateInterface;
+use Zend\Http\Request;
+use Zend\Mvc\MvcEvent;
+use Zend\Session\Container;
+
+class PageBuilderListener
+    implements ListenerAggregateInterface
+{
+    protected $listeners = array();
+
+    public function attach(EventManagerInterface $events)
+    {
+        $this->listeners[] = $events->attach(MvcEvent::EVENT_DISPATCH, array($this, 'initEntityManager'), 10);
+        $this->listeners[] = $events->attach(MvcEvent::EVENT_DISPATCH, array($this, 'initialiseWidgets'), -2);
+    }
+
+    public function detach(EventManagerInterface $events)
+    {
+        foreach ($this->listeners as $index => $listener) {
+            if ($events->detach($listener)) {
+                unset($this->listeners[$index]);
+            }
+        }
+    }
+
+    public function initialiseWidgets(MvcEvent $event)
+    {
+        if ($app = $event->getApplication()) {
+            $locator = $app->getServiceManager();
+            $config  = $locator->get('config');
+
+            if ($mainMenuKey = $config['pagebuilder']['main_navigation']) {
+
+                $viewHelperManager = $locator->get('viewHelperManager');
+
+                /** @var $navigation \Zend\View\Helper\Navigation */
+                $navigation = $viewHelperManager->get('navigation');
+
+                /** @var $menuTree \Zend\View\Helper\Navigation */
+                $menuTree  = $navigation($mainMenuKey);
+                $container = $menuTree->getContainer();
+
+                $activeMenu = $navigation->findActive($container);
+
+                if ($activeMenu) {
+                    /** @var $activeTheme \SynergyCommon\Entity\AbstractEntity */
+                    $activeTheme = $locator->get('active_theme');
+                    $menu        = $locator->get('pagebuilder\model\page')->findObject($activeMenu['page']->id);
+
+                    /** @var $pageBuilder \PageBuilder\View\Helper\PageBuilder */
+                    $pageBuilder = $viewHelperManager->get('buildPage');
+                    $pageBuilder->init($menu, $menuTree, $activeTheme);
+                }
+            }
+        }
+    }
+
+    public function initEntityManager(MvcEvent $e)
+    {
+        /** @var  $sm \Zend\Servicemanager\ServiceManager */
+        $sm = $e->getApplication()->getServiceManager();
+
+        /** @var $em \Doctrine\ORM\EntityManager */
+        $em = $sm->get('doctrine.entitymanager.orm_default');
+
+        /** @var $siteFilter \SynergyCommon\Doctrine\Filter\SiteFilter */
+        $siteFilter = $em->getFilters()->enable("site-specific");
+        $siteFilter->setServiceManager($sm);
+    }
+}
