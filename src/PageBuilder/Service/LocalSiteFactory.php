a72cc638b6f30cec0768e457af31887df9d57559
odiaseo <peleodiase@yahoo.co.uk>
2014-04-12 00:06:36 +0100

    bypass session

--- src/PageBuilder/Service/LocalSiteFactory.php
+++ src/PageBuilder/Service/LocalSiteFactory.php
@@ -5,7 +5,6 @@ use PageBuilder\Entity\Site;
 use Zend\Console\Request;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
-use Zend\Session\Container;
 
 class LocalSiteFactory
     implements FactoryInterface
@@ -13,10 +12,8 @@ class LocalSiteFactory
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
         $host = null;
-
         /** @var  $serviceLocator \Zend\Servicemanager\ServiceManager */
-        $containerKey = 'active-site';
-        $request      = $serviceLocator->get('application')->getRequest();
+        $request = $serviceLocator->get('application')->getRequest();
 
         if ($request instanceof Request) {
             /** @var $event \Zend\Mvc\MvcEvent */
@@ -31,24 +28,10 @@ class LocalSiteFactory
         }
         if ($host) {
             $hostname = str_replace(array('http://', 'https://', 'www.'), '', $host);
-            /** @var $container \ArrayObject */
-            $container = new Container();
-
-            if ($container->offsetExists($containerKey)) {
-                /** @var $siteModel \PageBuilder\Model\SiteModel */
-                $siteModel = $serviceLocator->get('pagebuilder\model\site');
-                $siteId    = $container->offsetGet($containerKey);
-                $site      = $siteModel->findObject($siteId);
-            } elseif (!$site = $serviceLocator->get('pagebuilder\model\site')->findOneByDomain($hostname)) {
-                header
-                (
-                    'HTTP/1.1 403 Application Error'
-                );
+            if (!$site = $serviceLocator->get('pagebuilder\model\site')->findOneByDomain($hostname)) {
+                header('HTTP/1.1 403 Application Error');
                 echo "Site is not registered";
                 exit;
-            } else {
-                /** @var $site \PageBuilder\Entity\Site */
-                $container->offsetSet($containerKey, $site->getId());
             }
         } else {
             $site = new Site();
