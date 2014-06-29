341a7a4b27d2ffdf75e003b6b2d49fd9d8f58ad7
odiaseo <peleodiase@yahoo.co.uk>
2014-06-29 14:04:01 +0100

    set accordion height

---
+++ src/PageBuilder/Model/SiteThemeModel.php
@@ -0,0 +1,37 @@
+<?php
+namespace PageBuilder\Model;
+
+use Doctrine\ORM\AbstractQuery;
+
+class SiteThemeModel extends BaseModel
+{
+
+    /**
+     * Get Active Site Theme
+     *
+     * @param $siteId
+     *
+     * @return \PageBuilder\Entity\Join\SiteTheme
+     */
+    public function getActiveSiteTheme($siteId)
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        /** @var $query \Doctrine\ORM\Query */
+
+        $query = $qb->select('e')
+            ->from($this->_entity, 'e')
+            ->where('e.siteId = :id')
+            ->andWhere('e.isActive = 1')
+            ->setMaxResults(1)
+            ->setParameter('id', $siteId)
+            ->getQuery();
+
+        /** @var $result \PageBuilder\Entity\Join\SiteTheme */
+        $result = $query->getOneOrNullResult(AbstractQuery::HYDRATE_OBJECT);
+        if ($result) {
+            return $result->getThemeId();
+        } else {
+            return $result;
+        }
+    }
+}
