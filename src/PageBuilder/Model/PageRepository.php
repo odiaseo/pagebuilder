9d22916cd20200efe1045cf7e2ca0d2cd2e2cd48
odiaseo <peleodiase@yahoo.co.uk>
2014-08-11 01:03:14 +0100

    Added page translations

--- src/PageBuilder/Model/PageRepository.php
+++ src/PageBuilder/Model/PageRepository.php
@@ -3,10 +3,10 @@ namespace PageBuilder\Model;
 
 use Doctrine\ORM\AbstractQuery;
 use Doctrine\ORM\Query\QueryException;
-use Gedmo\Tree\Entity\Repository\NestedTreeRepository;
+use SynergyCommon\Model\NestedSetRepository;
 use SynergyCommon\ModelTrait\LocalAwareNestedSetTrait;
 
-class PageRepository extends NestedTreeRepository
+class PageRepository extends NestedSetRepository
 {
     use LocalAwareNestedSetTrait;
 }
