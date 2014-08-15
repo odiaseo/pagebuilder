d6364fc4192060983b9098b3ce53e0de8b757ab7
odiaseo <peleodiase@yahoo.co.uk>
2014-08-15 21:11:01 +0100

    Added component translations

--- src/PageBuilder/Model/ComponentModel.php
+++ src/PageBuilder/Model/ComponentModel.php
@@ -11,4 +11,20 @@ use SynergyCommon\ModelTrait\LocaleAwareTrait;
 class ComponentModel extends BaseModel
 {
     use LocaleAwareTrait;
+
+    public function findObject($id)
+    {
+        $query = $this->getEntityManager()
+            ->createQueryBuilder()
+            ->select('e')
+            ->from($this->_entity, 'e')
+            ->where('e.id = :id')
+            ->setParameter(':id', $id)
+            ->setMaxResults(1)
+            ->getQuery();
+
+        $query = $this->addHints($query);
+
+        return $query->getOneOrNullResult();
+    }
 }
