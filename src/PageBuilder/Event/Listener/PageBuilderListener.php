86f30352c159d84c5106da28d8ef2c77a7ef96f7
odiaseo <peleodiase@yahoo.co.uk>
2014-08-25 13:38:30 +0100

    GEt error page for site

--- src/PageBuilder/Event/Listener/PageBuilderListener.php
+++ src/PageBuilder/Event/Listener/PageBuilderListener.php
@@ -21,6 +21,7 @@ class PageBuilderListener
 
     public function attach(EventManagerInterface $events)
     {
+
         $this->listeners[] = $events->attach(MvcEvent::EVENT_DISPATCH, array($this, 'initialiseWidgets'), -2);
         $this->listeners[] = $events->attach(
             array(
@@ -43,11 +44,7 @@ class PageBuilderListener
 
     public function renderErrorPage()
     {
-        /** @var $model \pageBuilder\Model\PageModel */
-        $model = $this->_serviceManager->get('pagebuilder\model\page');
-
-        /** @var $errorPage \SynergyCommon\Entity\BasePage */
-        $errorPage = $model->getRepository()->findOneBy(array('slug' => 'error-page'));
+        $errorPage = $this->findErrorPage();
 
         if ($errorPage) {
             /** @var $viewHelperManager \Zend\View\HelperPluginManager */
@@ -108,5 +105,39 @@ class PageBuilderListener
                 }
             }
         }
+    }
+
+    /**
+     * @return \SynergyCommon\Entity\BasePage
+     */
+    private function findErrorPage()
+    {
+        /** @var $viewHelperManager \Zend\View\HelperPluginManager */
+        $viewHelperManager = $this->_serviceManager->get('viewHelperManager');
+
+        /** @var $navigation \Zend\View\Helper\Navigation */
+        $navigation = $viewHelperManager->get('navigation');
+
+        /** @var $helper \PageBuilder\View\Helper\PageBuilder */
+        $helper = $viewHelperManager->get('buildPage');
+
+        /** @var $options \PageBuilder\View\Helper\Config\PageBuilderConfig */
+        $options = $helper->getOptions();
+
+        /** @var $menuTree \Zend\View\Helper\Navigation */
+        $menuTree = $navigation($options->getMainNavigation());
+
+        $errorPage = $menuTree->getContainer()->findOneBy('privilege', 'error-page');
+        /** @var $model \pageBuilder\Model\PageModel */
+        $model = $this->_serviceManager->get('pagebuilder\model\page');
+
+        if (empty($errorPage)) {
+            /** @var $errorPage \SynergyCommon\Entity\BasePage */
+            $errorPage = $model->findOneBy(array('slug' => 'error-page'));
+        } else {
+            $errorPage = $model->findObject($errorPage->getId());
+        }
+
+        return $errorPage;
     }
 }
