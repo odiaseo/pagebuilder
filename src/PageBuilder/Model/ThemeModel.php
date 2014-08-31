70ba50f15cd0b0796e513fc2902a65de784d8e2d
odiaseo <peleodiase@yahoo.co.uk>
2014-08-31 23:27:20 +0100

    Added site type table

--- src/PageBuilder/Model/ThemeModel.php
+++ src/PageBuilder/Model/ThemeModel.php
@@ -2,6 +2,7 @@
 namespace PageBuilder\Model;
 
 use Doctrine\ORM\AbstractQuery;
+use Doctrine\ORM\NoResultException;
 
 class ThemeModel extends BaseModel
 {
@@ -15,13 +16,12 @@ class ThemeModel extends BaseModel
             ->innerJoin('t.siteThemes', 's')
             ->where('s.siteId = :id')
             ->andWhere('s.isActive = 1')
-            ->setMaxResults(1)
             ->setParameter(':id', $siteId)
             ->getQuery();
 
         try {
-            return $query->getOneOrNullResult(AbstractQuery::HYDRATE_OBJECT) ? : array();
-        } catch (\Exception $e) {
+            return $query->getSingleResult(AbstractQuery::HYDRATE_OBJECT);
+        } catch (NoResultException $e) {
             return false;
         }
     }
