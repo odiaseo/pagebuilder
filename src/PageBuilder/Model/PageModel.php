95a59cf32d5e32f865eb2b76f95ffbdfe11988ad
odiaseo <peleodiase@yahoo.co.uk>
2014-11-09 16:26:34 +0000

    add i18n to page properties

--- src/PageBuilder/Model/PageModel.php
+++ src/PageBuilder/Model/PageModel.php
@@ -11,204 +11,186 @@ use SynergyCommon\ModelTrait\LocaleAwareTrait;
  *
  * @package PageBuilder\Model
  */
-class PageModel extends BaseModel
-{
-    use LocaleAwareTrait;
+class PageModel extends BaseModel {
+	use LocaleAwareTrait;
 
-    /**
-     * Returns the navigation menus
-     *
-     * @param int $rootLevel
-     *
-     * @return mixed
-     */
-    public function getTreeStructure($rootLevel = 1)
-    {
-        return $this->getRepository()->getTreeStructure($rootLevel);
-    }
+	/**
+	 * Returns the navigation menus
+	 *
+	 * @param int $rootLevel
+	 *
+	 * @return mixed
+	 */
+	public function getTreeStructure( $rootLevel = 1 ) {
+		return $this->getRepository()->getTreeStructure( $rootLevel );
+	}
 
-    public function getRootMenu()
-    {
-        return $this->getRepository()->getRootMenu();
-    }
+	public function getRootMenu() {
+		return $this->getRepository()->getRootMenu();
+	}
 
-    public function getRootMenuById($pageId)
-    {
-        return $this->getRepository()->getRootMenuById($pageId);
-    }
+	public function getRootMenuById( $pageId ) {
+		return $this->getRepository()->getRootMenuById( $pageId );
+	}
 
-    public function nestify($arrs, $depthKey = 'level')
-    {
-        return $this->getRepository()->nestify($arrs, $depthKey);
-    }
+	public function nestify( $arrs, $depthKey = 'level' ) {
+		return $this->getRepository()->nestify( $arrs, $depthKey );
+	}
 
-    public function getNavigationContainer($menus, $routeMatch)
-    {
-        return $this->getRepository()->getNavigationContainer($menus, $routeMatch);
-    }
+	public function getNavigationContainer( $menus, $routeMatch ) {
+		return $this->getRepository()->getNavigationContainer( $menus, $routeMatch );
+	}
 
-    public function toHierarchy($collection, $childKey = 'pages')
-    {
-        $trees    = $this->getRepository()->toHierarchy($collection, $childKey, array($this, 'buildPage'));
-        $entities = $this->buildEntityTree();
+	public function toHierarchy( $collection, $childKey = 'pages' ) {
+		$trees    = $this->getRepository()->toHierarchy( $collection, $childKey, array( $this, 'buildPage' ) );
+		$entities = $this->buildEntityTree();
 
-        if ($entities) {
-            array_push($trees, $entities);
-        }
+		if ( $entities ) {
+			array_push( $trees, $entities );
+		}
 
-        return $trees;
-    }
+		return $trees;
+	}
 
-    protected function buildEntityTree()
-    {
-        return array();
+	protected function buildEntityTree() {
+		return array();
 
-    }
+	}
 
-    public static function buildPage($node, $hasIdentity = false)
-    {
-        $params = array();
-        if ($node['parameters']) {
-            parse_str($node['parameters'], $params);
-        }
+	public static function buildPage( $node, $hasIdentity = false ) {
+		$params = array();
+		if ( $node['parameters'] ) {
+			parse_str( $node['parameters'], $params );
+		}
 
-        if (in_array($node['slug'], array('login', 'logout'))) {
-            if ($node['slug'] == 'login') {
-                $node['isVisible'] = !$hasIdentity;
-            } elseif ($node['slug'] == 'logout') {
-                $node['isVisible'] = $hasIdentity;
-            }
-        }
+		if ( in_array( $node['privilege'], array( 'login', 'logout' ) ) ) {
+			if ( $node['privilege'] == 'login' ) {
+				$node['isVisible'] = ! $hasIdentity;
+			} elseif ( $node['privilege'] == 'logout' ) {
+				$node['isVisible'] = $hasIdentity;
+			}
+		}
 
-        $menu = array(
-            'id'        => $node['id'],
-            'title'     => $node['title'],
-            'label'     => empty($node['label']) ? $node['title'] : $node['label'],
-            'route'     => $node['routeName'],
-            'resource'  => self::getResourceString($node['routeName'], $node['slug']),
-            'privilege' => $node['privilege'],
-            'visible'   => $node['isVisible'],
-            'level'     => $node['level'],
-            'icon'      => $node['iconClassName'],
-            'params'    => $params
-        );
+		$menu = array(
+			'id'        => $node['id'],
+			'title'     => $node['title'],
+			'label'     => empty( $node['label'] ) ? $node['title'] : $node['label'],
+			'route'     => $node['routeName'],
+			'resource'  => self::getResourceString( $node['routeName'], $node['slug'] ),
+			'privilege' => $node['privilege'],
+			'visible'   => $node['isVisible'],
+			'level'     => $node['level'],
+			'i18n'      => $node['slug'],
+			'icon'      => $node['iconClassName'],
+			'params'    => $params
+		);
 
-        if (!empty($node['uri'])) {
-            $menu['uri']    = $node['uri'];
-            $menu['target'] = '_blank';
-            unset($menu['route']);
-        }
+		if ( ! empty( $node['uri'] ) ) {
+			$menu['uri']    = $node['uri'];
+			$menu['target'] = '_blank';
+			unset( $menu['route'] );
+		}
 
-        return $menu;
-    }
+		return $menu;
+	}
 
-    /**
-     * @param $slug
-     *
-     * @return array
-     */
-    public function getBreadcrumbPath($slug)
-    {
-        return $this->getRepository()->getBreadcrumbPath($slug);
-    }
+	/**
+	 * @param $slug
+	 *
+	 * @return array
+	 */
+	public function getBreadcrumbPath( $slug ) {
+		return $this->getRepository()->getBreadcrumbPath( $slug );
+	}
 
-    public function getDetails($pageId)
-    {
-        $page = $this->getRepository()->find($pageId);
+	public function getDetails( $pageId ) {
+		$page = $this->getRepository()->find( $pageId );
 
-        $details           = $page->toArray();
-        $details['layout'] = is_object($page->template) ? $page->template->layout : '';
+		$details           = $page->toArray();
+		$details['layout'] = is_object( $page->template ) ? $page->template->layout : '';
 
-        return $details;
+		return $details;
 
-    }
+	}
 
-    public function updateTemplateId($pageId, $templateId)
-    {
-        $page             = $this->getRepository()->find($pageId);
-        $page->templateId = $templateId;
-        $res              = $this->save($page);
+	public function updateTemplateId( $pageId, $templateId ) {
+		$page             = $this->getRepository()->find( $pageId );
+		$page->templateId = $templateId;
+		$res              = $this->save( $page );
 
-        return $res;
-    }
+		return $res;
+	}
 
-    public function getNavigation()
-    {
-        $navigation = $this->_sm->get('viewhelpermanager')->get('navigation');
-        $menu       = $navigation('menu');
+	public function getNavigation() {
+		$navigation = $this->_sm->get( 'viewhelpermanager' )->get( 'navigation' );
+		$menu       = $navigation( 'menu' );
 
-        return $menu;
-    }
+		return $menu;
+	}
 
-    public function listRootMenusByTitle()
-    {
-        $list  = array();
-        $roots = $this->getRoots();
-        foreach ($roots as $item) {
-            $list[$item['id']] = array(
-                'title'       => $item['title'],
-                'description' => $item['description']
-            );
-        }
+	public function listRootMenusByTitle() {
+		$list  = array();
+		$roots = $this->getRoots();
+		foreach ( $roots as $item ) {
+			$list[ $item['id'] ] = array(
+				'title'       => $item['title'],
+				'description' => $item['description']
+			);
+		}
 
-        return $list;
-    }
+		return $list;
+	}
 
-    public function getRoots($mode = AbstractQuery::HYDRATE_ARRAY)
-    {
-        $qb = $this->getEntityManager()->createQueryBuilder();
+	public function getRoots( $mode = AbstractQuery::HYDRATE_ARRAY ) {
+		$qb = $this->getEntityManager()->createQueryBuilder();
 
-        /** @var $query \Doctrine\ORM\Query */
-        $query = $qb->select('e')
-            ->from($this->_entity, 'e')
-            ->where($qb->expr()->eq('e.level', ':level'))
-            ->setParameter('level', 0)
-            ->getQuery();
+		/** @var $query \Doctrine\ORM\Query */
+		$query = $qb->select( 'e' )
+		            ->from( $this->_entity, 'e' )
+		            ->where( $qb->expr()->eq( 'e.level', ':level' ) )
+		            ->setParameter( 'level', 0 )
+		            ->getQuery();
 
-        $result = $query->execute(null, $mode);
+		$result = $query->execute( null, $mode );
 
-        return $result;
-    }
+		return $result;
+	}
 
-    public function getPageDetails($pageId)
-    {
-        $page = $this->getRepository()->find($pageId);
+	public function getPageDetails( $pageId ) {
+		$page = $this->getRepository()->find( $pageId );
 
-        $details           = $page->toArray();
-        $details['layout'] = is_object($page->template) ? $page->template->layout : '';
+		$details           = $page->toArray();
+		$details['layout'] = is_object( $page->template ) ? $page->template->layout : '';
 
-        return $details;
+		return $details;
 
-    }
+	}
 
-    public function getActivePages()
-    {
-        $qb = $this->getEntityManager()->createQueryBuilder();
+	public function getActivePages() {
+		$qb = $this->getEntityManager()->createQueryBuilder();
 
-        /** @var $query \Doctrine\ORM\Query */
-        $query = $qb->select('e')
-            ->from($this->_entity, 'e')
-            ->where('e.isVisible = 1')
-            ->andWhere('e.parent NOT IN (12, 15)')
-            ->andWhere('e.level > 0')
-            ->getQuery();
+		/** @var $query \Doctrine\ORM\Query */
+		$query = $qb->select( 'e' )
+		            ->from( $this->_entity, 'e' )
+		            ->where( 'e.isVisible = 1' )
+		            ->andWhere( 'e.parent NOT IN (12, 15)' )
+		            ->andWhere( 'e.level > 0' )
+		            ->getQuery();
 
-        $result = $query->execute();
+		$result = $query->execute();
 
-        return $result;
-    }
+		return $result;
+	}
 
-    public static function getResourceString($routeName, $uniqueId)
-    {
-        return 'mvc:' . strtolower($routeName) . '.' . strtolower($uniqueId);
-    }
+	public static function getResourceString( $routeName, $uniqueId ) {
+		return 'mvc:' . strtolower( $routeName ) . '.' . strtolower( $uniqueId );
+	}
 
-    public function getPages()
-    {
-        /** @var $repo \Gedmo\Tree\Entity\Repository\NestedTreeRepository */
-        $repo  = $this->getRepository(); //  $em->getRepository($navService->getEntity());
-        $menus = $repo->getNodesHierarchy();
+	public function getPages() {
+		/** @var $repo \Gedmo\Tree\Entity\Repository\NestedTreeRepository */
+		$repo  = $this->getRepository(); //  $em->getRepository($navService->getEntity());
+		$menus = $repo->getNodesHierarchy();
 
-        return $this->toHierarchy($menus);
-    }
+		return $this->toHierarchy( $menus );
+	}
 }
