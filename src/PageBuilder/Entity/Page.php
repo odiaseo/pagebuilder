4dc575dd81ff817d99d287101085785c8305560f
odiaseo <peleodiase@yahoo.co.uk>
2014-12-22 12:43:07 +0000

    moved resource entities

--- src/PageBuilder/Entity/Page.php
+++ src/PageBuilder/Entity/Page.php
@@ -15,115 +15,136 @@ use SynergyCommon\Entity\BasePage;
  * @Gedmo\Tree(type="nested")
  * @Gedmo\TranslationEntity(class="PageBuilder\Entity\PageTranslation")
  */
-class Page
-    extends BasePage
-{
-    /**
-     * @ORM\ManyToOne(targetEntity="PageBuilder\Entity\Template")
-     * @ORM\JoinColumn(name="template_id", referencedColumnName="id", nullable=true)
-     */
-    protected $template;
-    /**
-     * @Gedmo\TreeParent
-     * @ORM\ManyToOne(targetEntity="PageBuilder\Entity\Page", inversedBy="children", fetch="LAZY", cascade={"persist"})
-     * @ORM\JoinColumn(name="parent_id", referencedColumnName="id", nullable=true)
-     */
-    protected $parent;
-    /**
-     * @ORM\OneToMany(targetEntity="PageBuilder\Entity\Page", mappedBy="parent")
-     * @ORM\OrderBy({"title" = "ASC"})
-     */
-    protected $children;
-    /**
-     * @ORM\OneToMany(targetEntity="PageBuilder\Entity\Join\PageTheme", mappedBy="pageId" , cascade={"persist"})
-     * @ORM\JoinTable(name="Page_Theme")
-     */
-    protected $pageThemes;
-    /**
-     * @ORM\OneToMany(targetEntity="PageBuilder\Entity\Site", mappedBy="rootPage")
-     */
-    protected $sites;
-    /**
-     * @ORM\OneToMany(
-     *   targetEntity="PageTranslation",
-     *   mappedBy="object",
-     *   cascade={"persist", "remove"}
-     * )
-     */
-    protected $translations;
+class Page extends BasePage {
+	/**
+	 * @ORM\ManyToOne(targetEntity="PageBuilder\Entity\Template")
+	 * @ORM\JoinColumn(name="template_id", referencedColumnName="id", nullable=true)
+	 */
+	protected $template;
+	/**
+	 * @Gedmo\TreeParent
+	 * @ORM\ManyToOne(targetEntity="PageBuilder\Entity\Page", inversedBy="children", fetch="LAZY", cascade={"persist"})
+	 * @ORM\JoinColumn(name="parent_id", referencedColumnName="id", nullable=true)
+	 */
+	protected $parent;
+	/**
+	 * @ORM\OneToMany(targetEntity="PageBuilder\Entity\Page", mappedBy="parent")
+	 * @ORM\OrderBy({"title" = "ASC"})
+	 */
+	protected $children;
+	/**
+	 * @ORM\OneToMany(targetEntity="PageBuilder\Entity\Join\PageTheme", mappedBy="pageId" , cascade={"persist"})
+	 * @ORM\JoinTable(name="Page_Theme")
+	 */
+	protected $pageThemes;
+	/**
+	 * @ORM\ManyToMany(targetEntity="Resource", cascade="persist", fetch="LAZY")
+	 * @ORM\JoinTable(name="Page_Resource")
+	 */
+	protected $resources;
+	/**
+	 * @ORM\OneToMany(targetEntity="PageBuilder\Entity\Site", mappedBy="rootPage")
+	 */
 
-    public function __construct()
-    {
-        parent::__construct();
-        $this->children     = new ArrayCollection();
-        $this->pageThemes   = new ArrayCollection();
-        $this->sites        = new ArrayCollection();
-        $this->translations = new ArrayCollection();
-    }
+	protected $sites;
+	/**
+	 * @ORM\OneToMany(
+	 *   targetEntity="PageTranslation",
+	 *   mappedBy="object",
+	 *   cascade={"persist", "remove"}
+	 * )
+	 */
+	protected $translations;
+
+	public function __construct() {
+		parent::__construct();
+		$this->children     = new ArrayCollection();
+		$this->pageThemes   = new ArrayCollection();
+		$this->sites        = new ArrayCollection();
+		$this->translations = new ArrayCollection();
+		$this->resources    = new ArrayCollection();
+	}
 
 
-    public function addTranslation(PageTranslation $t)
-    {
-        if (!$this->translations->contains($t)) {
-            $this->translations[] = $t;
-            $t->setObject($this);
-        }
-    }
+	public function addTranslation( PageTranslation $t ) {
+		if ( ! $this->translations->contains( $t ) ) {
+			$this->translations[] = $t;
+			$t->setObject( $this );
+		}
+	}
 
-    public function setSites($sites)
-    {
-        $this->sites = $sites;
-    }
+	/**
+	 * @return mixed
+	 */
+	public function getResources() {
+		return $this->resources;
+	}
 
-    public function getSites()
-    {
-        return $this->sites;
-    }
+	/**
+	 * @param mixed $resources
+	 */
+	public function setResources( $resources ) {
+		$this->resources = $resources;
+	}
 
-    public function setChildren($children)
-    {
-        $this->children = $children;
-    }
+	/**
+	 * @return mixed
+	 */
+	public function getTranslations() {
+		return $this->translations;
+	}
 
-    public function getChildren()
-    {
-        return $this->children;
-    }
+	/**
+	 * @param mixed $translations
+	 */
+	public function setTranslations( $translations ) {
+		$this->translations = $translations;
+	}
 
-    public function setPageThemes($pageThemes)
-    {
-        $this->pageThemes = $pageThemes;
-    }
+	public function setSites( $sites ) {
+		$this->sites = $sites;
+	}
 
-    public function getPageThemes()
-    {
-        return $this->pageThemes;
-    }
+	public function getSites() {
+		return $this->sites;
+	}
 
-    public function setParent($parent)
-    {
-        $this->parent = $parent;
-    }
+	public function setChildren( $children ) {
+		$this->children = $children;
+	}
 
-    /**
-     * @return \PageBuilder\Entity\Page
-     */
-    public function getParent()
-    {
-        return $this->parent;
-    }
+	public function getChildren() {
+		return $this->children;
+	}
 
-    public function setTemplate($template)
-    {
-        $this->template = $template;
-    }
+	public function setPageThemes( $pageThemes ) {
+		$this->pageThemes = $pageThemes;
+	}
 
-    /**
-     * @return \PageBuilder\Entity\Template
-     */
-    public function getTemplate()
-    {
-        return $this->template;
-    }
+	public function getPageThemes() {
+		return $this->pageThemes;
+	}
+
+	public function setParent( $parent ) {
+		$this->parent = $parent;
+	}
+
+	/**
+	 * @return \PageBuilder\Entity\Page
+	 */
+	public function getParent() {
+		return $this->parent;
+	}
+
+	public function setTemplate( $template ) {
+		$this->template = $template;
+	}
+
+	/**
+	 * @return \PageBuilder\Entity\Template
+	 */
+	public function getTemplate() {
+		return $this->template;
+	}
 
 }
---
+++ src/PageBuilder/Entity/ResourceType.php
@@ -0,0 +1,80 @@
+<?php
+namespace PageBuilder\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use SynergyCommon\Entity\AbstractEntity;
+
+/**
+ * Resource Type
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="Resource_Type")
+ *
+ */
+class ResourceType extends AbstractEntity {
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer");
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	protected $id;
+	/**
+	 * @ORM\Column(type="string")
+	 */
+	protected $title;
+	/**
+	 * @ORM\Column(type="string")
+	 */
+	protected $description;
+	/**
+	 * @ORM\Column(type="string", name="icon_class_name")
+	 */
+	protected $iconClassName = 'icon-th';
+	/**
+	 * @Gedmo\Slug(fields={"title"})
+	 * @ORM\Column(type="string")
+	 */
+	protected $slug;
+
+	public function setDescription( $description ) {
+		$this->description = $description;
+	}
+
+	public function getDescription() {
+		return $this->description;
+	}
+
+	public function setIconClassName( $iconClassName ) {
+		$this->iconClassName = $iconClassName;
+	}
+
+	public function getIconClassName() {
+		return $this->iconClassName;
+	}
+
+	public function setId( $id ) {
+		$this->id = $id;
+	}
+
+	public function getId() {
+		return $this->id;
+	}
+
+	public function setSlug( $slug ) {
+		$this->slug = $slug;
+	}
+
+	public function getSlug() {
+		return $this->slug;
+	}
+
+	public function setTitle( $title ) {
+		$this->title = $title;
+	}
+
+	public function getTitle() {
+		return $this->title;
+	}
+
+}
