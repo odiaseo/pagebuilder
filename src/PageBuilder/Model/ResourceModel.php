ac48f7a9902a6d58b05d74f56fc6c0e94c0d7e53
odiaseo <peleodiase@yahoo.co.uk>
2014-12-22 12:57:58 +0000

    moved resource models

---
+++ src/PageBuilder/Model/ResourceModel.php
@@ -0,0 +1,30 @@
+<?php
+namespace PageModel\Model;
+
+use SynergyCommon\Model\AbstractModel;
+
+/**
+ * Class ResourceModel
+ *
+ * @package PageModel\Model
+ */
+class ResourceModel extends AbstractModel {
+	/**
+	 * List Generic Resources
+	 *
+	 * @param int $limit
+	 *
+	 * @return mixed
+	 */
+	public function getGenericResources( $limit = 10 ) {
+		$query = $this->getEntityManager()
+		              ->createQueryBuilder()
+		              ->select( 'e' )
+		              ->from( $this->_entity, 'e' )
+		              ->where( 'e.isGeneric = 1' )
+		              ->setMaxResults( $limit )
+		              ->getQuery();
+
+		return $query->execute();
+	}
+}
---
+++ src/PageBuilder/Model/ResourceTypeModel.php
@@ -0,0 +1,13 @@
+<?php
+namespace PageModel\Model;
+
+use SynergyCommon\Model\AbstractModel;
+
+/**
+ * Class ResourceTypeModel
+ *
+ * @package AffiliateManager\Model
+ */
+class ResourceTypeModel extends AbstractModel {
+
+}
