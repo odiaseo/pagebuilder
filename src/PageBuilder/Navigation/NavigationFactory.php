03955d3a5929a433adcae8c27b14c6669f36c556
Pele Odiase <pele.odiase@valtech.co.uk>
2015-01-19 02:01:09 +0000

    change where em is initialised

--- src/PageBuilder/Navigation/NavigationFactory.php
+++ src/PageBuilder/Navigation/NavigationFactory.php
@@ -4,48 +4,58 @@ namespace PageBuilder\Navigation;
 use Zend\Navigation\Exception;
 use Zend\Navigation\Navigation;
 use Zend\Navigation\Service\DefaultNavigationFactory;
+use Zend\ServiceManager\ServiceLocatorInterface;
 
-class NavigationFactory extends DefaultNavigationFactory {
-	protected $_sm;
-	protected $_model = 'pagebuilder\model\page';
+/**
+ * Class NavigationFactory
+ *
+ * @package PageBuilder\Navigation
+ */
+class NavigationFactory extends DefaultNavigationFactory
+{
+    protected $_sm;
+    protected $_model = 'pagebuilder\model\page';
 
+    protected function getName()
+    {
+        return 'pagebuilder\menu';
+    }
 
-	protected function getName() {
-		return 'pagebuilder\menu';
-	}
+    protected function getPagesFromConfig($config = null)
+    {
+        if (is_callable($config)) {
+            return $config($this->_sm);
+        }
+    }
 
-	protected function getPagesFromConfig( $config = null ) {
-		if ( is_callable( $config ) ) {
-			return $config( $this->_sm );
-		}
-	}
+    protected function getPages(ServiceLocatorInterface $serviceManager)
+    {
+        /** @var $pageModel \PageBuilder\Model\PageModel */
+        $pageModel = $serviceManager->get($this->_model);
 
-	protected function getPages( $serviceManager ) {
-		/** @var $pageModel \PageBuilder\Model\PageModel */
-		$pageModel = $serviceManager->get( $this->_model );
+        /** @var $repo \Gedmo\Tree\Entity\Repository\NestedTreeRepository */
+        $repo  = $pageModel->getRepository(); //  $em->getRepository($navService->getEntity());
+        $menus = $repo->getNodesHierarchy();
 
-		/** @var $repo \Gedmo\Tree\Entity\Repository\NestedTreeRepository */
-		$repo  = $pageModel->getRepository(); //  $em->getRepository($navService->getEntity());
-		$menus = $repo->getNodesHierarchy();
+        $pages       = $pageModel->toHierarchy($menus);
+        $this->pages = $this->preparePages($serviceManager, $pages);
 
-		$pages       = $pageModel->toHierarchy( $menus );
-		$this->pages = $this->preparePages( $serviceManager, $pages );
+        return $this->pages;
+    }
 
-		return $this->pages;
-	}
+    public function getNavigationByRootId($id)
+    {
+        /** @var $pageModel \PageBuilder\Model\PageModel */
+        $pageModel = $this->_sm->get($this->_model);
 
-	public function getNavigationByRootId( $id ) {
-		/** @var $pageModel \PageBuilder\Model\PageModel */
-		$pageModel = $this->_sm->get( $this->_model );
+        /** @var $repo \Gedmo\Tree\Entity\Repository\NestedTreeRepository */
+        $repo     = $pageModel->getRepository();
+        $rootMenu = $pageModel->getRootMenuById($id);
+        $menus    = $repo->getNodesHierarchy($rootMenu);
 
-		/** @var $repo \Gedmo\Tree\Entity\Repository\NestedTreeRepository */
-		$repo     = $pageModel->getRepository();
-		$rootMenu = $pageModel->getRootMenuById( $id );
-		$menus    = $repo->getNodesHierarchy( $rootMenu );
+        $pages = $pageModel->toHierarchy($menus);
+        $pages = $this->preparePages($this->_sm, $pages);
 
-		$pages = $pageModel->toHierarchy( $menus );
-		$pages = $this->preparePages( $this->_sm, $pages );
-
-		return new Navigation( $pages );
-	}
+        return new Navigation($pages);
+    }
 }
