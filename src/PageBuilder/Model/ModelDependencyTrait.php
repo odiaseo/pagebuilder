52feef12213c4b3bd07d63c6554ea2671ace117f
Pele Odiase <pele.odiase@valtech.co.uk>
2015-01-25 14:54:02 +0000

    operator precedence fix

--- src/PageBuilder/Model/ModelDependencyTrait.php
+++ src/PageBuilder/Model/ModelDependencyTrait.php
@@ -7,49 +7,51 @@ use SynergyCommon\Doctrine\CachedEntityManager;
 use SynergyCommon\Model\AbstractModel;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
-trait ModelDependencyTrait {
+trait ModelDependencyTrait
+{
 
-	/**
-	 * @param ServiceLocatorInterface $serviceLocator
-	 * @param AbstractModel           $model
-	 * @param                         $entity
-	 *
-	 * @return BaseModel
-	 */
-	protected function setDependency( ServiceLocatorInterface $serviceLocator, AbstractModel $model, $entity ) {
+    /**
+     * @param ServiceLocatorInterface $serviceLocator
+     * @param AbstractModel           $model
+     * @param                         $entity
+     *
+     * @return BaseModel
+     */
+    protected function setDependency(ServiceLocatorInterface $serviceLocator, AbstractModel $model, $entity)
+    {
 
-		/** @var EntityManager $entityManager */
-		$cacheStatus   = $serviceLocator->get( 'synergy\cache\status' );
-		$entityManager = $serviceLocator->get( 'doctrine.entitymanager.' . $model->getOrm() );
-		$cachedManager = new CachedEntityManager( $entityManager, $cacheStatus->enabled );
+        /** @var EntityManager $entityManager */
+        $cacheStatus   = $serviceLocator->get('synergy\cache\status');
+        $entityManager = $serviceLocator->get('doctrine.entitymanager.' . $model->getOrm());
+        $cachedManager = new CachedEntityManager($entityManager, $cacheStatus->enabled);
 
-		if ( $serviceLocator->has( 'zfcuser_auth_service' ) ) {
-			$authService = $serviceLocator->get( 'zfcuser_auth_service' );
-			$identity    = $authService->hasIdentity();
-			$model->setIdentity( $authService->getIdentity() );
-		} else {
-			$identity = false;
-		}
+        if ($serviceLocator->has('zfcuser_auth_service')) {
+            $authService = $serviceLocator->get('zfcuser_auth_service');
+            $identity    = $authService->hasIdentity();
+            $model->setIdentity($authService->getIdentity());
+        } else {
+            $identity = false;
+        }
 
-		if ( is_string( $entity ) ) {
-			$model->setEntity( $entity );
-		} elseif ( $model instanceof BaseModel ) {
-			$model->setEntityInstance( $entity );
-			$model->setEntity( get_class( $entity ) );
-		}
+        if (is_string($entity)) {
+            $model->setEntity($entity);
+        } elseif ($model instanceof BaseModel) {
+            $model->setEntityInstance($entity);
+            $model->setEntity(get_class($entity));
+        }
 
-		if ( $model instanceof LocaleAwareInterface ) {
-			$site = $serviceLocator->get('active\site');
-			if ( $locale = $site->getLocale() ) {
-				$model->setLocale( $locale );
-			}
-		}
+        if ($model instanceof LocaleAwareInterface) {
+            $site = $serviceLocator->get('active\site');
+            if ($locale = $site->getLocale()) {
+                $model->setLocale($locale);
+            }
+        }
 
-		$enabled = ( ! $identity and $cacheStatus->enabled );
-		$model->setEnableResultCache( $enabled );
-		$model->setLogger( $serviceLocator->get( 'logger' ) );
-		$model->setEntityManager( $cachedManager );
+        $enabled = (false == $identity and $cacheStatus->enabled);
+        $model->setEnableResultCache($enabled);
+        $model->setLogger($serviceLocator->get('logger'));
+        $model->setEntityManager($cachedManager);
 
-		return $model;
-	}
+        return $model;
+    }
 }
