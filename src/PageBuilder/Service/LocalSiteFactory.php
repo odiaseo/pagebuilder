026260d4ffd937d9b0543957a408d15356e77de4
Pele Odiase <pele.odiase@valtech.co.uk>
2016-03-05 11:07:45 +0000

    auto updating linked repo

--- src/PageBuilder/Service/LocalSiteFactory.php
+++ src/PageBuilder/Service/LocalSiteFactory.php
@@ -6,6 +6,7 @@ use PageBuilder\Model\SiteModel;
 use SynergyCommon\Exception\MissingArgumentException;
 use SynergyCommon\Model\AbstractModel;
 use SynergyCommon\ModelTrait\LocaleAwareTrait;
+use SynergyCommon\Util;
 use Zend\Authentication\AuthenticationService;
 use Zend\Authentication\Storage\Session;
 use Zend\Console\Request;
@@ -32,38 +33,25 @@ class LocalSiteFactory implements FactoryInterface
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
         /** @var  $serviceLocator \Zend\Servicemanager\ServiceManager */
-        $host      = null;
-        $isConsole = false;
-        $request   = $serviceLocator->get('application')->getRequest();
-        $event     = $serviceLocator->get('application')->getMvcEvent();
+        $request = $serviceLocator->get('application')->getRequest();
+        $event   = $serviceLocator->get('application')->getMvcEvent();
+
         //IMportant to run this first so that the session is initialised
         $manager = $this->initialiseSessionManager($serviceLocator, $request);
 
-        if ($request instanceof Request) {
-            /** @var $event \Zend\Mvc\MvcEvent */
-            $isConsole = true;
-            /** @var $rm \Zend\Mvc\Router\RouteMatch */
-            if ($rm = $event->getRouteMatch()) {
-                $host = $rm->getParam('host', $rm->getParam(self::CLIENT_DOMAIN_KEY, null));
-            }
-        } else {
-            /** @var $request \Zend\Http\PhpEnvironment\Request */
-            $host = $request->getServer('HTTP_HOST', $request->getQuery(self::CLIENT_DOMAIN_KEY, null));
-        }
+        list($isConsole, $hostname) = Util::getDomainFromRequest($request, $event);
 
-        if ($host) {
+        if ($hostname) {
             /** @var SiteModel $model */
-            $config = $serviceLocator->get('config');
-            list($host,) = explode(':', $host);
-            $globalDomain = $this->cleanDomain($config['pagebuilder']['global_domain']);
-            $hostname     = $this->cleanDomain($host);
+            $config       = $serviceLocator->get('config');
+            $globalDomain = Util::cleanDomain($config['pagebuilder']['global_domain']);
             $model        = $serviceLocator->get('pagebuilder\model\site');
 
             if (!$site = $model->findSiteBy(array('domain' => $hostname))) {
                 $message = "Site is not registered";
-                $serviceLocator->get('logger')->warn($host . ' - domain was requested but not found');
-                if (!$isConsole and $host != $globalDomain) {
-                    $destination = sprintf('http://www.%s?nfh=%s', $globalDomain, $host);
+                $serviceLocator->get('logger')->warn($hostname . ' - domain was requested but not found');
+                if (!$isConsole and $hostname != $globalDomain) {
+                    $destination = sprintf('http://www.%s?nfh=%s', $globalDomain, $hostname);
                     header('HTTP/1.1 302 Domain not found');
                     header('Location: ' . $destination);
                 } else {
@@ -83,7 +71,7 @@ class LocalSiteFactory implements FactoryInterface
             $i18nLocale = $site->getLocale();
         }
         $container->offsetSet(AbstractModel::SESSION_LOCALE_KEY, $i18nLocale);
-        $container->offsetSet(AbstractModel::SESSION_SITE_KEY, $host);
+        $container->offsetSet(AbstractModel::SESSION_SITE_KEY, $hostname);
 
         //Set locale
         \setlocale(LC_ALL, $site->getLocale());
@@ -97,28 +85,9 @@ class LocalSiteFactory implements FactoryInterface
     }
 
     /**
-     * @param $domain
-     *
-     * @return mixed
-     */
-    protected function cleanDomain($domain)
-    {
-        $hackReplacements = [
-            //'www.ww.',
-            //'www.w.',
-            // 'ww.w.',
-            // 'ww.',
-            // 'doc1000.',
-            'admin.'
-        ];
-
-        $domain = str_replace(array('http://', 'https://', 'www.'), '', $domain);
-
-        return str_replace($hackReplacements, '', $domain);
-    }
-
-    /**
      * @param $serviceLocator
+     * @param $request
+     * @return null
      */
     private function initialiseSessionManager($serviceLocator, $request)
     {
